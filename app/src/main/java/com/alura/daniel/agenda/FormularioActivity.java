42af77097577cdf7895fd108ace6b633e15f1618
DaanMoura <daniel.lucio.moura@gmail.com>
2017-08-03 11:04:21 -0300

    new options on context menu and camera button on form activity

--- app/src/main/java/com/alura/daniel/agenda/FormularioActivity.java
+++ app/src/main/java/com/alura/daniel/agenda/FormularioActivity.java
@@ -1,6 +1,13 @@
 package com.alura.daniel.agenda;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaDataSource;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.graphics.BitmapCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,14 +15,19 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.alura.daniel.agenda.dao.AlunoDAO;
 import com.alura.daniel.agenda.modelo.Aluno;
 
+import java.io.File;
+
 public class FormularioActivity extends AppCompatActivity {
 
+    public static final int CAMERA_CODE = 12;
     private FormularioHelper helper;
+    private String caminhoFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +40,30 @@ public class FormularioActivity extends AppCompatActivity {
         Aluno aluno = (Aluno) intent.getSerializableExtra("aluno");
         if (aluno != null){
             helper.preencheFormulario(aluno);
+        }
+
+        Button botaoFoto = (Button) findViewById(R.id.botao_foto);
+        botaoFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent tirarFoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                caminhoFoto = getExternalFilesDir(null) + "/" + System.currentTimeMillis() + ".jpg";
+                File arquivoFoto = new File(caminhoFoto);
+                tirarFoto.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(arquivoFoto));
+                startActivityForResult(tirarFoto, CAMERA_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CAMERA_CODE && resultCode == Activity.RESULT_OK){
+            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+            ImageView foto = (ImageView) findViewById(R.id.formulario_foto);
+            bitmap = Bitmap.createScaledBitmap(bitmap, 300, 300, true);
+            foto.setImageBitmap(bitmap);
+            foto.setScaleType(ImageView.ScaleType.FIT_XY);
         }
     }
 
--- app/src/main/java/com/alura/daniel/agenda/ListaAlunosActivity.java
+++ app/src/main/java/com/alura/daniel/agenda/ListaAlunosActivity.java
@@ -1,9 +1,12 @@
 package com.alura.daniel.agenda;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 //import android.support.design.widget.FloatingActionButton;
 //import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.ContextMenu;
 import android.view.MenuItem;
@@ -12,13 +15,12 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.alura.daniel.agenda.dao.AlunoDAO;
 import com.alura.daniel.agenda.modelo.Aluno;
 
-import java.io.Serializable;
 import java.util.List;
+import java.util.jar.Manifest;
 //import android.support.v7.widget.Toolbar;
 //import android.view.View;
 //import android.view.Menu;
@@ -62,7 +64,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
         List<Aluno> alunos = dao.buscaAlunos();
         dao.close();
 
-        ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
+        ArrayAdapter<Aluno> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos);
         listaAlunos.setAdapter(adapter);
     }
 
@@ -74,14 +76,53 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        final Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
+
+        MenuItem itemLigar = menu.add("Ligar");
+        itemLigar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(ActivityCompat.checkSelfPermission(ListaAlunosActivity.this, android.Manifest.permission.CALL_PHONE)
+                        != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(ListaAlunosActivity.this,
+                            new String[]{android.Manifest.permission.CALL_PHONE}, 123);
+                }
+                else{
+                    Intent intentLigar = new Intent(Intent.ACTION_CALL);
+                    intentLigar.setData(Uri.parse("tel:"+aluno.getTelefone()));
+                    startActivity(intentLigar);
+                }
+                return false;
+            }
+        });
+
+
+        MenuItem itemSMS = menu.add("SMS");
+        Intent intentSMS = new Intent(Intent.ACTION_VIEW);
+        intentSMS.setData(Uri.parse("sms:"+aluno.getTelefone()));
+        itemSMS.setIntent(intentSMS);
+
+        MenuItem itemVisitarSite = menu.add("Visitar site");
+        Intent intentSite = new Intent(Intent.ACTION_VIEW);
+        String site = aluno.getSite();
+        if(!site.startsWith("http://")){
+            site = "http://" + site;
+        }
+        intentSite.setData(Uri.parse(site));
+        itemVisitarSite.setIntent(intentSite);
+
+        MenuItem itemMapa = menu.add("Achar no Mapa");
+        Intent intentMapa = new Intent(Intent.ACTION_VIEW);
+        intentMapa.setData(Uri.parse("geo:0,0?="+aluno.getEndereco()));
+        itemMapa.setIntent(intentMapa);
+
         MenuItem deletar = menu.add("Deletar");
         deletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener(){
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
-
                 AlunoDAO dao = new AlunoDAO(ListaAlunosActivity.this);
+
                 dao.deleta(aluno);
                 dao.close();
                 carregaLista();
